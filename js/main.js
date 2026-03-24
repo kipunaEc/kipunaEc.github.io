421841552ea13d6e40c9542026454ed26c70d4f8
kipunaEc <83312347+kipunaEc@users.noreply.github.com>
2026-03-23 20:55:33 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 fetch('components/sidebar.html')
-  .then(res => res.text())
+  .then(res => {
+    if (!res.ok) {
+      throw new Error("No se pudo cargar el sidebar");
+    }
+    return res.text();
+  })
   .then(data => {
     document.getElementById('sidebar').innerHTML = data;
+  })
+  .catch(error => {
+    console.error(error);
   });
