16407679c5a88d9897c0b74f5d06e42237356333
kipunaEc <83312347+kipunaEc@users.noreply.github.com>
2026-05-12 20:59:15 -0500

    Delete js directory

--- js/post.js
+++
@@ -1,8 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-const nombrePost = params.get("post");
-
-fetch(`data/blog/${nombrePost}`)
-  .then(res => res.text())
-  .then(md => {
-    document.getElementById("post").innerHTML = marked.parse(md);
-  });
